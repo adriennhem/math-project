51571389293dc4d6acd7e0a39dd25f6873220c0a
adrien nhem <adrien.nhem@gmail.com>
2021-01-23 19:44:42 +0000

    update high score

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     var currentQuestion;
+    var highScore = 0;
     var interval;
     var timeLeft = 10;
     var score = 0;
@@ -13,6 +14,12 @@ $(document).ready(function(){
       score += amount;
       $('#score').text(score);
     };
+
+    var updateHighScore = function (score) {
+      if (score > highScore) {
+        $('#high-score').text(score);
+      }
+    }
     
     var startGame = function () {
       if (!interval) {
@@ -25,6 +32,7 @@ $(document).ready(function(){
           if (timeLeft === 0) {
             clearInterval(interval);
             interval = undefined;
+            updateHighScore(score);
           }
         }, 1000);  
       }
@@ -33,14 +41,34 @@ $(document).ready(function(){
     var randomNumberGenerator = function (size) {
       return Math.ceil(Math.random() * size);
     };
+
+    var randomOperatorGenerator = function () {
+      var operators = [];
+      $('input[type=checkbox]').each(function (index) {
+        if ($('input[type=checkbox]')[index].checked) {
+          operators.push($('input[type=checkbox]')[index].value)
+        }
+      })
+      return operators[Math.floor(Math.random() * operators.length)]
+    }
     
     var questionGenerator = function () {
       var question = {};
       var num1 = randomNumberGenerator(10);
       var num2 = randomNumberGenerator(10);
+      var operator = randomOperatorGenerator();
+
+      if (operator == "+") {
+        question.answer = num1 + num2;
+      } else if (operator == "-") {
+        question.answer = num1 - num2;
+      } else if (operator == "*") {
+        question.answer = num1 * num2;
+      } else {
+        question.answer = num1 / num2;
+      }
       
-      question.answer = num1 + num2;
-      question.equation = String(num1) + " + " + String(num2);
+      question.equation = String(num1) + " " + String(operator) + " " + String(num2);
       
       return question;
     };
